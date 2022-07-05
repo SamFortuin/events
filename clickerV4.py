f43fe543b9e544e4a1ebbca10ec58592065ecc75
Sam Fortuin <99059050@mydavinci.nl>
2022-07-05 02:05:12 +0200

    finished clickerV4

---
+++ clickerV4.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("300x200")
+root.title('Clicker V4')
+root.configure(bg="#808080")
+
+def colorChange(newColor): 
+    root.configure(bg=newColor)
+
+def counterChange(changeBy):
+    global number, latch
+    number += changeBy
+    print(number)
+    counter.configure(text=number)
+    if changeBy > 0:
+        latch = True
+    elif changeBy < 0:
+        latch = False
+    if number == 0:
+        colorChange("#808080")
+    elif number > 0:
+        colorChange("#008400")
+    else:
+        colorChange("#ff0000")
+
+def counterMath(a):
+    global number,latch
+    if latch:
+        number *= 3
+        counter.configure(text=number)
+    elif not latch:
+        number /= 3
+        counter.configure(text=number)
+number = 0
+latch = None
+
+btnUp = tk.Button(root)
+btnUp.config(text="up",width=30,command=lambda: counterChange(1))
+btnUp.place(relx=0.5,rely=0.25,anchor='center')
+
+counter = tk.Label(root)
+counter.configure(width=30,text=number)
+counter.bind("<Enter>",lambda a:colorChange('#ffff00'))
+counter.bind("<Leave>",lambda a:counterChange(0))
+counter.bind("<Double-Button-1>",lambda a: counterMath('a'))
+counter.place(relx=0.5,rely=0.5,anchor='center')
+
+btnDown = tk.Button(root)
+btnDown.bind()
+btnDown.config(text="down",width=30,command=lambda: counterChange(-1))
+btnDown.place(relx=0.5,rely=0.75,anchor='center')
+
+root.bind("<Up>", lambda a: counterChange(1))
+root.bind("=", lambda a: counterChange(1))
+root.bind("<Down>", lambda a: counterChange(-1))
+root.bind("-", lambda a: counterChange(-1))
+root.bind("<swwaapace>", lambda a: counterMath("a"))
+
+
+root.mainloop()
