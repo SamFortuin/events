a32145b7b1f885e54018e6988d703d78709f7f1a
Sam Fortuin <99059050@mydavinci.nl>
2022-07-12 17:02:50 +0200

    added placing new labels after key is pressed

--- fpsTrainer.py
+++ fpsTrainer.py
@@ -12,19 +12,29 @@ root.geometry("600x400")
 
 def placeLabel():
     keyPresses = ['w','a','s','d','<space>','<Button-1>','<Double-Button-1>','<Triple-Button-1>']
+    global lbl
     lbl = tk.Label()
     lbl.configure(bg='red',height=3,textvariable=pressString)
     randX = randint(1,91) / 100
     randY = randint(1,87) / 100
     lbl.place(relx=randX,rely=randY)
     currentKey = choice(keyPresses)
-    cleanedKey = capwords(currentKey).replace('<','').replace('>','')
-    lbl.configure(width=6) if len(currentKey) <= 1 else None
+    cleanedKey = capwords(currentKey[1:-1]).replace('-',' ') if "<" in currentKey else currentKey.upper()
+    lbl.configure(width=6) if len(currentKey) <= 1 or currentKey == '<space>' else None
     pressString.set(cleanedKey)
-    root.bind(f'{currentKey}',lambda a: lbl.destroy())
+    root.bind(f'{currentKey}',lambda a: newLabel(currentKey))
+    return currentKey
 
-a = 'simp'
-pressString = tk.StringVar(root,value=a)
-placeLabel()
+def newLabel(key):
+    global lbl
+    root.unbind(key)
+    lbl.destroy()
+    placeLabel()
+
+placeHolderString = 'a'
+pressString = tk.StringVar(root,value=placeHolderString)
+a = placeLabel()
+# root.bind("<Key>",lambda a: print(a.split('=')))
+# print(a)
 
 root.mainloop()
